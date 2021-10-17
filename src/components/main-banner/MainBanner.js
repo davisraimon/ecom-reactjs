b79829b15160ac70d84c3f31100c46303df634e9
Davis Raimon <davisraimon@gmail.com>
2021-10-17 12:58:42 +0530

    banner changes

--- src/components/main-banner/MainBanner.js
+++ src/components/main-banner/MainBanner.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import "./MainBanner.css";
 
 export default class MainBanner extends Component {
   render() {
-    return <div>Banner</div>;
+    return (
+      <div className="main-banner">
+        <img src={this.props.src}></img>
+      </div>
+    );
   }
 }
